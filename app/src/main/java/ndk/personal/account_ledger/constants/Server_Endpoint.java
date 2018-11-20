55de8be3eecb8db0b849955852dfb2e46e6912ff
Banee Ishaque K <baneeishaque@gmail.com>
2018-11-21 00:34:11 +0530

    K

--- app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
@@ -1,7 +1,10 @@
 package ndk.personal.account_ledger.constants;
 
 public class Server_Endpoint {
-    public static String SERVER_IP_ADDRESS = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server";
+
+    //    public static String SERVER_IP_ADDRESS = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server";
+    public static String SERVER_IP_ADDRESS = "http://139.59.65.128/account_ledger_server";
+
     public static String BUILD_FOLDER = "builds";
     public static String MODULE_NAME = "app";
     public static String APK_PREFIX = "-debug.apk";
