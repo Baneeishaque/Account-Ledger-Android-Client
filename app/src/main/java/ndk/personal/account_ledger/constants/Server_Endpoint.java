6acd81734d0bf7f06020d7e8e0f37b54c8013fd9
Banee Ishaque K <Baneeishaque@gmail.com>
2019-09-28 20:06:55 +0530

    adgarage

--- app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
@@ -2,11 +2,7 @@ package ndk.personal.account_ledger.constants;
 
 public class Server_Endpoint {
 
-<<<<<<< HEAD
     public static String SERVER_IP_ADDRESS = "http://adgarage.co/wp-production/account_ledger_server";
-=======
-    public static String SERVER_IP_ADDRESS = "http://192.168.43.89/php/Account_Ledger_Server";
->>>>>>> 37696de65430c55f092b29b19d6d4917e0424f98
 
     public static String BUILD_FOLDER = "builds";
     public static String MODULE_NAME = "app";
