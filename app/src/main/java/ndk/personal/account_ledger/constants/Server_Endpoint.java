abd929c378cfa0187aa6f5460f03682b0b45589b
Banee Ishaque K <Baneeishaque@gmail.com>
2019-09-28 12:11:30 +0530

    adgarage

--- app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
@@ -2,8 +2,7 @@ package ndk.personal.account_ledger.constants;
 
 public class Server_Endpoint {
 
-    //    public static String SERVER_IP_ADDRESS = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/account_ledger_server";
-    public static String SERVER_IP_ADDRESS = "http://139.59.65.128/account_ledger_server";
+    public static String SERVER_IP_ADDRESS = "http://adgarage.co/wp-production/account_ledger_server";
 
     public static String BUILD_FOLDER = "builds";
     public static String MODULE_NAME = "app";
