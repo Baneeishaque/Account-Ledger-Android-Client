fe17882ba1c21fab0d1b717e183a28a79f7abdd6
Banee Ishaque K <Baneeishaque@gmail.com>
2019-10-25 01:03:09 +0530

    Update Server_Endpoint.java

--- app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
@@ -2,13 +2,16 @@ package ndk.personal.account_ledger.constants;
 
 public class Server_Endpoint {
 
-    public static String SERVER_IP_ADDRESS = "http://adgarage.co/wp-production/account_ledger_server";
+    // public static String SERVER_IP_ADDRESS = "http://adgarage.co/wp-production/account_ledger_server";
+    public static String SERVER_IP_ADDRESS = "http://account-ledger-server.herokuapp.com";
 
+    public static String HTTP_API_FOLDER = "http_API";
+    public static String FILE_EXTENSION = ".php";
+    
     public static String BUILD_FOLDER = "builds";
     public static String MODULE_NAME = "app";
     public static String APK_PREFIX = "-debug.apk";
     public static String UPDATE_URL = SERVER_IP_ADDRESS + "/" + BUILD_FOLDER + "/" + MODULE_NAME + APK_PREFIX;
-    public static String HTTP_API_FOLDER = "http_API";
-    public static String FILE_EXTENSION = ".php";
+    
 }
 
