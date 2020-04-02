e9f27ac08de5620e676f045c8158286bdef912c4
Banee Ishaque K <Baneeishaque@gmail.com>
2020-04-02 09:16:51 +0000

    Update Server_Endpoint.java

--- app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/Server_Endpoint.java
@@ -3,8 +3,8 @@ package ndk.personal.account_ledger.constants;
 public class Server_Endpoint {
 
     // public static String SERVER_IP_ADDRESS = "http://adgarage.co/wp-production/account_ledger_server";
-//    public static String SERVER_IP_ADDRESS = "http://account-ledger-server.herokuapp.com";
-    public static String SERVER_IP_ADDRESS = "http://ssquared.qa/cgi-bin/account_ledger_server";
+    public static String SERVER_IP_ADDRESS = "http://account-ledger-server.herokuapp.com";
+    // public static String SERVER_IP_ADDRESS = "http://ssquared.qa/cgi-bin/account_ledger_server";
     public static String HTTP_API_FOLDER = "http_API";
     public static String FILE_EXTENSION = ".php";
 
