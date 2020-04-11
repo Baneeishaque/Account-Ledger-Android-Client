85de7e6a98f7370104df02d7bb7352ab0b1cbbb3
Banee Ishaque K <baneeishaque@gmail.com>
2020-04-11 18:59:06 +0000

    last
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- app/src/main/java/ndk/personal/account_ledger/constants/API_Wrapper.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/API_Wrapper.java
@@ -6,6 +6,7 @@ import ndk.utils_android16.API_Utils;
 public class API_Wrapper {
 
     public static String get_http_API(String method_name) {
+
         //TODO : Eliminate Method name from API_Utils
         return API_Utils.get_http_API(method_name, Server_Endpoint.SERVER_IP_ADDRESS, Server_Endpoint.HTTP_API_FOLDER, Server_Endpoint.FILE_EXTENSION);
     }
