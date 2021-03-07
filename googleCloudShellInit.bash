3f05bf03f819d426d51d0b492e9b811e1201ae76
Banee Ishaque K <baneeishaque@gmail.com>
2021-03-07 14:41:08 +0000

    ZSH start up command, script permission fix

--- googleCloudShellInit.bash
+++ googleCloudShellInit.bash
@@ -1,4 +1,6 @@
+# TODO : check for existing android sdk folder
 cd $HOME && wget "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip" && unzip "commandlinetools-linux-6858069_latest.zip" && mkdir -p android-sdk-linux/cmdline-tools && mv cmdline-tools latest && mv latest android-sdk-linux/cmdline-tools/ && rm commandlinetools-linux-6858069_latest.zip
+# TODO : check for existing of required packages
 yes | android-sdk-linux/cmdline-tools/latest/bin/sdkmanager --licenses && android-sdk-linux/cmdline-tools/latest/bin/sdkmanager "platforms;android-30" "build-tools;30.0.3"
 export ANDROID_SDK_ROOT="$HOME/android-sdk-linux"
 export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools"
@@ -15,4 +17,5 @@ cd $HOME  && git clone "https://github.com/gradle/gradle-completion" ".oh-my-zsh
 cd $HOME  && git clone "https://github.com/bobthecow/git-flow-completion" ".oh-my-zsh/custom/plugins/git-flow-completion/"
 cd $HOME  && wget -P ".oh-my-zsh/custom/plugins/git-completion/" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"  && wget -P ".oh-my-zsh/custom/plugins/git-completion/" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.tcsh"  && wget -O ".oh-my-zsh/custom/plugins/git-completion/git-completion.plugin.zsh" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"  && wget -P ".oh-my-zsh/custom/plugins/git-completion/" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
 sed -i 's/plugins=(git)/plugins=(git gradle gradle-completion adb sdk common-aliases dircycle dirhistory dirpersist history copydir copyfile autojump fd git-completion git-auto-status git-prompt gitfast gitignore git-flow git-flow-completion git-flow-avh git-hubflow git-lfs git-extras last-working-dir per-directory-history perms wd safe-paste thefuck systemadmin scd pj magic-enter man command-not-found jump timer colored-man-pages jsontools grc colorize ripgrep httpie sprunge nmap transfer universalarchivecatimg extract)/g' ~/.zshrc
+echo "cd Account-Ledger-Android-Client" > ~/.zshenv
 zsh
