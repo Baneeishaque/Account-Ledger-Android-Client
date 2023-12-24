fd7ed8b3dda207ada2336c25f01f926a2b46fbdf
Banee Ishaque K <Baneeishaque@gmail.com>
2023-12-24 08:48:35 +0530

    Update ServerEndpoint.java

--- app/src/main/java/ndk/personal/account_ledger/constants/ServerEndpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/ServerEndpoint.java
@@ -2,7 +2,7 @@ package ndk.personal.account_ledger.constants;
 
 public class ServerEndpoint {
 
-    public static final String SERVER_ADDRESS = "http://account-ledger.professionalstudycentre.com";
+    public static final String SERVER_ADDRESS = "https://nomadller.in";
     public static final String HTTP_API_FOLDER = "http_api";
     public static final String FILE_EXTENSION = ".php";
 
