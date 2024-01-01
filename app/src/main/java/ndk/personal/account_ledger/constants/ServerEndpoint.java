c7755635b143919ea98c2e8626b65a6afb555250
Banee Ishaque K <baneeishaque@gmail.com>
2024-01-02 00:56:06 +0530

    Update ServerEndpoint.java

--- app/src/main/java/ndk/personal/account_ledger/constants/ServerEndpoint.java
+++ app/src/main/java/ndk/personal/account_ledger/constants/ServerEndpoint.java
@@ -6,10 +6,11 @@ public class ServerEndpoint {
     public static final String HTTP_API_FOLDER = "http_api";
     public static final String FILE_EXTENSION = ".php";
 
-    public static final String BUILD_FOLDER = "builds";
     public static final String MODULE_NAME = "app";
     public static final String APK_PREFIX = "-production-release.apk";
 
-    public static final String BUILD_SERVER_ADDRESS = SERVER_ADDRESS;
-    public static final String UPDATED_APK_URL = BUILD_SERVER_ADDRESS.equals(SERVER_ADDRESS) ? BUILD_SERVER_ADDRESS + "/" + MODULE_NAME + APK_PREFIX : SERVER_ADDRESS + "/" + BUILD_FOLDER + "/" + MODULE_NAME + APK_PREFIX;
+//    public static final String BUILD_FOLDER = "builds";
+//    public static final String BUILD_SERVER_ADDRESS = SERVER_ADDRESS;
+//    public static final String UPDATED_APK_URL = BUILD_SERVER_ADDRESS.equals(SERVER_ADDRESS) ? BUILD_SERVER_ADDRESS + "/" + MODULE_NAME + APK_PREFIX : SERVER_ADDRESS + "/" + BUILD_FOLDER + "/" + MODULE_NAME + APK_PREFIX;
+    public static final String UPDATED_APK_URL = SERVER_ADDRESS + "/" + MODULE_NAME + APK_PREFIX;
 }
