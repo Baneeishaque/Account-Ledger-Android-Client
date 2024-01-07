7fa2b853685e8fd226606e9bddbbd49b11eda828
Banee Ishaque K <baneeishaque@gmail.com>
2024-01-07 06:46:40 +0530

    checkUnsignedIntValueIncludingAsTextOnSharedPreferences

--- app/src/main/java/ndk/personal/account_ledger/utils/AccountLedgerExceptionUtils.java
+++ app/src/main/java/ndk/personal/account_ledger/utils/AccountLedgerExceptionUtils.java
@@ -3,12 +3,11 @@ package ndk.personal.account_ledger.utils;
 import android.content.Context;
 
 import ndk.personal.account_ledger.constants.ApplicationSpecification;
-import ndk.utils_android1.ExceptionUtils1;
+import ndk.utils_android19.ExceptionUtils19;
 
 public class AccountLedgerExceptionUtils {
     public static void handleExceptionOnGui(Context applicationContext, String exceptionDetails) {
 
-        ExceptionUtils1.handleExceptionOnGui( applicationContext, ApplicationSpecification.APPLICATION_NAME, exceptionDetails);
+        ExceptionUtils19.handleExceptionOnGui(applicationContext, ApplicationSpecification.APPLICATION_NAME, exceptionDetails);
     }
 }
-
